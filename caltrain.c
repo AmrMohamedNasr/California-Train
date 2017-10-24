5ae2f87002ca6bc61281445b249de2927d214ff6
Amr Mohamed Nasr <re13amr@gmail.com>
2017-10-24 13:22:30 +0200

    documented header

--- caltrain.c
+++ caltrain.c
@@ -1,51 +1,53 @@
 #include <pthread.h>
 #include "caltrain.h"
-#include <stdio.h>
 
-
+// Implementation (Documented in headers).
 void station_init(struct station *station) {
 	// Check for error.
-	pthread_mutex_init(&station->waiting_passengers_mutex, NULL);
+	pthread_mutex_init(&station->counters_lock, NULL);
+	pthread_cond_init(&station->waiting_passengers, NULL);
+	pthread_cond_init(&station->train_boarded, NULL);
 	station->waiting_passengers_count = 0;
 	station->train_empty_places = 0;
 	station->confirmed_on_train_count = 0;
-	pthread_cond_init(&station->waiting_passengers, NULL);
-	pthread_cond_init(&station->train_boarded, NULL);
 }
 
+// Implementation (Documented in headers).
 void station_load_train(struct station *station, int count) {
-	pthread_mutex_lock(&station->waiting_passengers_mutex);
+	pthread_mutex_lock(&station->counters_lock);
 	if (station->waiting_passengers_count != 0 && count != 0) {
 		station->train_empty_places = count;
 		station->confirmed_on_train_count = 0;
 		station->train_capacity = count;
 		pthread_cond_broadcast(&station->waiting_passengers);
-		pthread_cond_wait(&station->train_boarded, &station->waiting_passengers_mutex);
+		pthread_cond_wait(&station->train_boarded, &station->counters_lock);
 		station->waiting_passengers_count -= station->confirmed_on_train_count;	
 	}
-	pthread_mutex_unlock(&station->waiting_passengers_mutex);
+	pthread_mutex_unlock(&station->counters_lock);
 
 }
 
+// Implementation (Documented in headers).
 void station_wait_for_train(struct station *station) {
-	pthread_mutex_lock(&station->waiting_passengers_mutex);
+	pthread_mutex_lock(&station->counters_lock);
 	station->waiting_passengers_count++;
 	int boarded = 0;
 	while (boarded == 0) {
-		pthread_cond_wait(&station->waiting_passengers, &station->waiting_passengers_mutex);
+		pthread_cond_wait(&station->waiting_passengers, &station->counters_lock);
 		if (station->train_empty_places > 0) {
 			station->train_empty_places--;
 			boarded = 1;
 		} 
 	}
-	pthread_mutex_unlock(&station->waiting_passengers_mutex);
+	pthread_mutex_unlock(&station->counters_lock);
 }
 
+// Implementation (Documented in headers).
 void station_on_board(struct station *station) {
-	pthread_mutex_lock(&station->waiting_passengers_mutex);
+	pthread_mutex_lock(&station->counters_lock);
 	station->confirmed_on_train_count++;
 	if (station->confirmed_on_train_count == station->train_capacity || station->waiting_passengers_count == station->confirmed_on_train_count) {
 		pthread_cond_signal(&station->train_boarded);
 	}
-	pthread_mutex_unlock(&station->waiting_passengers_mutex);
+	pthread_mutex_unlock(&station->counters_lock);
 }
--- caltrain.h
+++ caltrain.h
@@ -1,19 +1,56 @@
 #include <pthread.h>
 
+// Define our station structure.
 struct station {
-	pthread_mutex_t waiting_passengers_mutex;
-	int waiting_passengers_count;
-	pthread_cond_t waiting_passengers;
-	pthread_cond_t train_boarded;
-	int train_empty_places;
-	int confirmed_on_train_count;
-	int train_capacity;
+	// MUTEX :
+		// the lock to keep our train counters synchronized.
+		pthread_mutex_t counters_lock;
+	// Conditions :
+		// Condition to keep waiting passengers on.
+		pthread_cond_t waiting_passengers;
+		// Condition to tell the train when it is full and all passengers that will ride it are confirmed to be on it.
+		pthread_cond_t train_boarded;
+	// Counters :
+		// The number of passengers waiting in the station.
+		int waiting_passengers_count;
+		// The current number of empty seats unclaimed in the train.
+		int train_empty_places;
+		// The number of passengers confirmed on train.
+		int confirmed_on_train_count;
+		// The current train stopping in the station capacity.
+		int train_capacity;
 };
-
+/**
+	- Initializes our station parameters.
+**/
 void station_init(struct station *station);
 
+/**
+	- Loads a train with waiting passengers in its free seats.
+	@count
+	- The number of empty seats in the train.
+	@station
+	- The station in which the train stops.
+	@return
+	- Will return only when the number of confirmed passengers that got on the train is
+	equal to the number of empty seats. If the train is already full or the station is empty,
+	it should return immediatly.
+**/
 void station_load_train(struct station *station, int count);
-
+/**
+	- Informs a station of the arrival of a new passenger.
+	@station
+	- The station in which the passenger will wait.
+	@return
+	- Will only return when the passenger gets an empty place on a train. However,
+	it will not inform the train that a passenger has arrived as he might be still
+	moving towards the train. 
+**/
 void station_wait_for_train(struct station *station);
-
+/**
+	- Informs the current train waiting in the station that a passenger has actually boarded
+	the train and got in an empty seat.
+	@return
+	- Immediatly. 
+**/
 void station_on_board(struct station *station);
